a40f410789fde963bd8882a04c89f3cf4dc0ca75
berylhu43 <beryl.hu@yahoo.com>
2023-11-01 00:44:33 -0500

    initial commit

---
+++ part1/kernel.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+### Functions for you to fill in ###
+
+
+
+def polynomial_kernel(X, Y, c, p):
+    """
+        Compute the polynomial kernel between two matrices X and Y::
+            K(x, y) = (<x, y> + c)^p
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            c - a coefficient to trade off high-order and low-order terms (scalar)
+            p - the degree of the polynomial kernel
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+    # YOUR CODE HERE
+    kernel_matrix = (np.dot(X,Y.T) +c) ** p
+    return kernel_matrix
+
+
+
+def rbf_kernel(X, Y, gamma):
+    """
+        Compute the Gaussian RBF kernel between two matrices X and Y::
+            K(x, y) = exp(-gamma ||x-y||^2)
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            gamma - the gamma parameter of gaussian function (scalar)
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+    # YOUR CODE HERE
+    # Calculate squared differences
+    squared_diffs = np.sum((X[:, np.newaxis, :] - Y[np.newaxis, :, :]) ** 2, axis=2)
+    # Compute the kernel matrix
+    kernel_matrix = np.exp(-gamma * squared_diffs)
+    
+    return kernel_matrix
